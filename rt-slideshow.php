0756da5dab29a87e901bd285f015cd0d173e6b17
Li8ning <jadejadharmraj5@gmail.com>
2023-02-18 15:48:58 +0530

    Initial Commit with Composer Setup

---
+++ rt-slideshow.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       RT Slideshow
+ * Plugin URI:        https://github.com/rtlearn/wpcs-Li8ning
+ * Description:       Create a beautiful slideshow.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Dharmrajsinh Jadeja
+ * Author URI:        https://blog.avgamingindia.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:
+ * Text Domain:       rt-slideshow
+ * Domain Path:       /languages
+ */
