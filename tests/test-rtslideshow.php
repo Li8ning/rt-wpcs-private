655642e16a309ee14ccdc1051c010660dc8568dc
Li8ning <jadejadharmraj5@gmail.com>
2023-03-15 19:06:34 +0530

    continue unit test

--- tests/test-rtslideshow.php
+++ tests/test-rtslideshow.php
@@ -5,31 +5,15 @@
  * @package RTSlideshow
  */
 
+ use RTSlideshow\RTSlideshow;
+
 class Test_rtslideshow extends WP_UnitTestCase {
+	
+	public function test_construct() {
 
-	protected $rt_slideshow;
+		$rtslidehsow = new RTSlideshow();
+		$this->assertEquals( 'rt-slideshow', $rtslidehsow->plugin_name );
 
-	public function setUp(): void {
-		parent::setUp();
-		$this->rt_slideshow = new RTSlideshow\RTSlideshow();
 	}
 
-	// public function test_image_ids_option_registered() {
-	// 	$this->assertTrue( get_option( 'rt_slideshow_image_ids' ) !== false );
-	// }
-
-	public function test_deactivation_hook_registered() {
-	    $this->assertTrue( has_action( 'deactivate/rt-slideshow.php', array( $this->rt_slideshow, 'deactivate' ) ) );
-	}
-
-	// public function test_uninstall_hook_registered() {
-	//     $this->assertTrue( has_action( 'uninstall_rt-slideshow/rt-slideshow.php', array( $this->rt_slideshow, 'uninstall' ) ) );
-	// }
-
-	// public function test_rewrite_rules_flushed() {
-    //     global $wp_rewrite;
-    //     $initial_structure = $wp_rewrite->permalink_structure;
-    //     $this->rt_slideshow->activate();
-    //     $this->assertNotEquals( $initial_structure, $wp_rewrite->permalink_structure );
-	// }
 }
